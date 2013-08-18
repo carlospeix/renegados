2c1cc1bd93585ffaa09dffaf55530017bc1c7cbb
Carlos Peix <carlos.peix@gmail.com>
2013-08-18 19:04:11 -0300

    Refactoring, introduciendo el array de tasas de interes

--- interes/model/liquidador.rb
+++ interes/model/liquidador.rb
@@ -1,17 +1,20 @@
 class Liquidador
-  def initialize(interes_diario = 0)
-    @interes_diario = interes_diario
+  def initialize(tasas_de_interes_diario)
+    @tasas_de_interes_diario = tasas_de_interes_diario
   end
 
   def interes(capital, fecha_vencimiento, fecha_liquidacion)
     return 0 if deuda_no_vencida(fecha_vencimiento, fecha_liquidacion)
 
-    return capital * (fecha_liquidacion - fecha_vencimiento).to_i / 100
+    tasa_interes = @tasas_de_interes_diario[0][2]
+    cantidad_de_dias = (fecha_liquidacion - fecha_vencimiento).to_i
+
+    return capital * cantidad_de_dias * tasa_interes / 100
   end
 
 private
   def deuda_no_vencida(fecha_vencimiento, fecha_liquidacion)
-    return fecha_vencimiento >= fecha_liquidacion
+    return fecha_vencimiento > fecha_liquidacion
   end
-  
+
 end
--- interes/spec/liquidador_spec.rb
+++ interes/spec/liquidador_spec.rb
@@ -4,14 +4,19 @@ require File.join(File.dirname(__FILE__),'../model/liquidador')
 
 describe Liquidador do
 
-  it 'debería calcular cero interés con capital no vencido' do
-    liquidador = Liquidador.new
-    liquidador.interes(100, Date.today, Date.today).should == 0
+  before(:each) do
+    tasas_interes = [
+      [ Date.new(2000, 1, 1), Date.new(2001, 1, 1), 2 ]
+    ]
+    @liquidador = Liquidador.new(tasas_interes)
   end
 
-  it 'debería calcular 2 pesos de interés para un día despues del vencimiento' do
-    liquidador = Liquidador.new(1)
-    liquidador.interes(200, Date.today-1, Date.today).should == 2
+  it 'debería calcular cero interés con capital no vencido' do
+    @liquidador.interes(100, Date.today, Date.today).should == 0
+  end
+
+  it 'debería calcular 4 pesos de interés para un día despues del vencimiento' do
+    @liquidador.interes(200, Date.today - 1, Date.today).should == 4
   end
 
 end
