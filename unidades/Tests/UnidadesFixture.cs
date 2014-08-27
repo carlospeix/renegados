66abee6dbd6a40e431a88bd27e76bbb3603cc81f
Carlos Peix <carlos.peix@gmail.com>
2014-08-27 18:19:06 -0300

    Proyecto vacio de unidades

---
+++ unidades/Tests/UnidadesFixture.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Renegados.Tests
+{
+	[TestFixture]
+	public class CasosEspecialesTest
+	{
+
+		[SetUp]
+		public void SetUp()
+		{
+		}
+
+		[Test]
+		public void NoReintegraPorExcederCantidadMensualAunqueMontoAlcance()
+		{
+		}
+	}
+}
